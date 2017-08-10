f292470426808c767776a1c4d7c4296f0479bbb1
SureCoin Dev2 <cryptoelastic@users.noreply.github.com>
2017-08-10 16:48:03 +0800

    Even support multiple arguments!!! How cool is that?

--- main.cpp
+++ main.cpp
@@ -2,9 +2,13 @@
 
 int main(int argc, const char *argv[])
 {
-    if (argc < 1) {
-        std::cout << "Hello, anonymous hipster!" << std::endl;
+    std::cout << "Hello,";
+    if (argc <= 1) {
+        std::cout << " anonymous hipster!";
     } else {
-        std::cout << "Hello, " << argv[0] << "!" << std::endl;
+        for (auto i = 1; i < argc; i++)
+            std::cout << " " << argv[i];
     }
+
+    std::cout << "!" << std::endl;
 }
